eb333755deb4d95e39ef0d298c6550730a7691ff
elena.stojanova <elena.stojanova@beyonnex.io>
2024-06-25 23:17:46 +0300

    add-single-learner-experiments
    add single learner experiments

---
+++ single-hfd-tree/runs/_sources/single_hfd_tree_3fc9b680c23c0ae9ce8c4e2d32a4449a.py
@@ -0,0 +1,70 @@
+
+import random
+from river import tree
+from river.datasets import synth
+from sacred.observers import FileStorageObserver
+from sacred.observers import MongoObserver
+
+from sacred import Experiment
+
+ex = Experiment(name="single_hfd")
+ex.observers.append(MongoObserver(url='mongodb://mongo_user:mongo_password@127.0.0.1:27017/sacred?authSource=admin',
+                                  db_name='sacred'))
+ex.observers.append(FileStorageObserver('../runs'))
+
+
+@ex.config
+def cfg():
+    nr_samples_train = 200
+    mask_probability = 0.5
+    nr_samples_test = 50
+    test_step = 20
+
+
+# not very efficient for start, but is OK for small datasets
+def generate_dataset_with_mask(_rnd, _seed, nr_samples: int, mask_prob: float):
+    orig = synth.SEA(variant=0, seed=_seed).take(nr_samples)
+    masked = []
+    for x, y in orig:
+        if _rnd.random() < mask_prob:
+            masked.append((x, None))
+        else:
+            masked.append((x, y))
+    return masked
+
+
+def train(_run, _rnd, data_set, test_step, learner, test_set, nr_samples_test):
+    step = 0
+    train_observed = 0
+    train_correct = 0
+    for x, y in data_set:
+        train_observed += 1
+        if y is not None:
+            y_pred = learner.predict_one(x)
+            if y_pred == y:
+                train_correct += 1
+            learner.learn_one(x, y)
+        step += 1
+        if step % test_step == 0:
+            test(_run, test_set, learner, step, nr_samples_test)
+        _run.log_scalar("train_observed", train_observed, step)
+        _run.log_scalar("train_correct", train_correct, step)
+
+
+def test(_run, test_set, learner, step_nr, nr_samples_test):
+    nr_correct = 0
+    for x, y in test_set:
+        y_pred = learner.predict_one(x)
+        if y == y_pred:
+            nr_correct += 1
+    learner_accuracy = nr_correct / nr_samples_test
+    _run.log_scalar("test_accuracy", learner_accuracy, step_nr)
+
+
+@ex.automain
+def run(_run, _seed, nr_samples_train, mask_probability, nr_samples_test, test_step):
+    random.seed(_seed)
+    learner = tree.HoeffdingTreeClassifier()
+    data_set = generate_dataset_with_mask(random, _seed, nr_samples_train, mask_probability)
+    test_set = list(synth.SEA(variant=0, seed=_seed).take(nr_samples_test))
+    train(_run, random, data_set, test_step, learner, test_set, nr_samples_test)
